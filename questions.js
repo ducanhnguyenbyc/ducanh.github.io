356acf05c9e3f791e206add61bee6fd97bb406dd
ducanhnguyenbyc <52854315+ducanhnguyenbyc@users.noreply.github.com>
2025-05-12 10:39:07 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,34 @@
+const questions = [
+    {
+      question: "Máy bay nào có thời gian hoạt động dài nhất?",
+      options: ["B-1B Lancer", "MQ-9 Reaper", "F-22 Raptor", "F/A-18 Super Hornet"],
+      answer: "MQ-9 Reaper"
+    },
+    {
+      question: "F-35 Lightning II là loại máy bay gì?",
+      options: ["Ném bom", "Tiêm kích đa nhiệm tàng hình", "Trinh sát điện tử", "Tuần tra biển"],
+      answer: "Tiêm kích đa nhiệm tàng hình"
+    },
+    {
+      question: "Máy bay nào có sải cánh lớn nhất?",
+      options: ["F-15 Eagle", "MQ-4C Triton", "B-1B Lancer", "E-2C Hawkeye"],
+      answer: "MQ-4C Triton"
+    },
+    {
+      question: "RC-135 Rivet Joint thuộc loại gì?",
+      options: ["Trinh sát điện tử", "Tiêm kích", "Ném bom", "Tuần tra biển"],
+      answer: "Trinh sát điện tử"
+    },
+    {
+      question: "F-22 Raptor có trần bay khoảng bao nhiêu?",
+      options: ["12.000 m", "15.000 m", "20.000 m", "25.000 m"],
+      answer: "20.000 m"
+    },
+    {
+      question: "X-47B là máy bay gì?",
+      options: ["UAV thử nghiệm", "Máy bay tiêm kích", "Ném bom tàng hình", "Cảnh báo sớm"],
+      answer: "UAV thử nghiệm"
+    }
+    // TODO: Thêm 194 câu nữa
+  ];
+  
